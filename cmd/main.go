f217b2703c188d3ba9f651e48f34e5c4712ced9c
Frederick <fthu0001@student.monash.edu>
2025-09-24 20:07:02 +1000

    added main.go

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"go-shortener/internal/api"
+	"go-shortener/internal/database"
+	"[github.com/gofiber/fiber/v2](https://github.com/gofiber/fiber/v2)"
+)
+
+func main() {
+	if err := database.Connect(); err != nil {
+		log.Fatalf("could not connect to databases: %v", err)
+	}
+
+	app := fiber.New()
+
+	api.SetupRoutes(app)
+
+	log.Fatal(app.Listen(":3000"))
+}
